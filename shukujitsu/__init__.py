a877a8abc2da8d22f771e46591cf26f865f45f70
Youhei Sakurai <sakurai.youhei@gmail.com>
2020-11-28 22:14:06 +0900

    fix issue #4

--- shukujitsu/__init__.py
+++ shukujitsu/__init__.py
@@ -2,7 +2,7 @@ from shukujitsu.exports import CountryHoliday
 from shukujitsu.exports import Japan
 
 
-__version__ = "2020.11.27.post1"
+__version__ = "2020.11.27.post2"
 # aliases
 JP = JPN = Japan
 assert __version__ and CountryHoliday and Japan and JP and JPN
