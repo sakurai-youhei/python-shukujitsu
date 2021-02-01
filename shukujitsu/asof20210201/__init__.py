ec3494978e2e4111018fd41616a3c793bf3fd514
Youhei Sakurai <sakurai.youhei@gmail.com>
2021-02-01 14:19:40 +0900

    https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv

---
+++ shukujitsu/asof20210201/__init__.py
@@ -0,0 +1,8 @@
+from pkgutil import get_data
+
+# https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv
+BIN = get_data("shukujitsu.asof20210201", "syukujitsu.csv")
+if BIN:
+    TXT = BIN.decode("cp932")
+else:
+    raise ImportError("Can't load syukujitsu.csv under {}".format(__name__))
