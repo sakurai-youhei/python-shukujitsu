9fe31f2ffa39fd129332d7d632ac7ef5436f717b
Youhei Sakurai <sakurai.youhei@gmail.com>
2021-11-06 10:34:43 +0900

    Bump up version

--- shukujitsu/__init__.py
+++ shukujitsu/__init__.py
@@ -2,7 +2,7 @@ from shukujitsu.exports import CountryHoliday
 from shukujitsu.exports import Japan
 
 
-__version__ = "2021.2.1"
+__version__ = "2021.2.1.post2"
 # aliases
 JP = JPN = Japan
 assert __version__ and CountryHoliday and Japan and JP and JPN
