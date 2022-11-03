a4997d219cdb31e3f89bdab57b18ede5be8a2e74
Youhei Sakurai <sakurai.youhei@gmail.com>
2022-11-03 09:11:25 +0900

    Add support of Python 3.11 #16

--- setup.py
+++ setup.py
@@ -49,4 +49,5 @@ setup(name="python-shukujitsu",
         Programming Language :: Python :: 3.8
         Programming Language :: Python :: 3.9
         Programming Language :: Python :: 3.10
+        Programming Language :: Python :: 3.11
       """).splitlines())
